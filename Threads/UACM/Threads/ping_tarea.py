bef17f99a1f4be08458261710654fbc4c97ed9f3
reneMX <rene.martinez.magana@estudiante.uacm.edu.mx>
2021-04-17 14:05:45 -0500

    Version 0.0.1

--- Threads/UACM/Threads/ping_tarea.py
+++ Threads/UACM/Threads/ping_tarea.py
@@ -3,10 +3,10 @@ import os
 import threading
 from random import randint
 local = threading.local()
-contador = 0
+contador = 1
 
 #host  = "192.168.31.126"
-host2 = "192.168.31.
+#host2 = "192.168.31."
 #respuesta = os.system("ping " + host) WINDOWS
 
 #respuesta = os.system("ping -c 3 " + host)
@@ -21,15 +21,26 @@ def run(local, barrier):
 	#valores de 1 hasta 126.
 	#local.my_value = randint(0, 10**2)
 	global contador
-	local.my_value = contador
+	global host2
+	host2 = "192.168.31."+str(contador)
+	#local.my_value = host2
 	contador += 1
+	respuesta = os.system("ping -c 3 " + host2)
+	local.my_value = host2
+	
+	if respuesta == 0:
+		print(host2, 'Esta Arriba')
+	else:
+		print(host2, 'Esta Abajo')
 	t = threading.current_thread()
 	print(f'Thread {t.name} has value {local.my_value}')
 	barrier.wait()
 	print(f'Thread {t.name} still value {local.my_value}')
 
 
-count = 129
+
+
+count = 126
 barrier = threading.Barrier(count)
 
 threads = [

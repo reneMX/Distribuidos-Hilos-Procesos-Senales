9d6d9390b6419b3c62bac36edb71e8c83baddeae
reneMX <rene.martinez.magana@estudiante.uacm.edu.mx>
2021-04-17 19:53:13 -0500

    Version 0.0.2

--- Threads/UACM/Threads/ping_tarea.py
+++ Threads/UACM/Threads/ping_tarea.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
 import os
 import threading
-from random import randint
+import time
 local = threading.local()
-contador = 1
-
-#host  = "192.168.31.126"
-#host2 = "192.168.31."
-#respuesta = os.system("ping " + host) WINDOWS
-
-#respuesta = os.system("ping -c 3 " + host)
-
-#if respuesta == 0:
-#   print( host, 'esta arriba')
-#else:
-#   print(host, 'esta abajo')
-
+contador = 0
 
 def run(local, barrier):
-	#valores de 1 hasta 126.
-	#local.my_value = randint(0, 10**2)
+	#valores de 1 hasta 126
 	global contador
-	global host2
-	host2 = "192.168.31."+str(contador)
-	#local.my_value = host2
+	global host
+	host = "192.168.31." + str(contador)
 	contador += 1
-	respuesta = os.system("ping -c 3 " + host2)
-	local.my_value = host2
-	
-	if respuesta == 0:
-		print(host2, 'Esta Arriba')
-	else:
-		print(host2, 'Esta Abajo')
-	t = threading.current_thread()
-	print(f'Thread {t.name} has value {local.my_value}')
-	barrier.wait()
-	print(f'Thread {t.name} still value {local.my_value}')
+	respuesta = os.system("ping -c 3 " + host)
 
+	local.my_value = contador
+
+	if respuesta == 0:
+		print(host, 'Esta Arriba')
+	else:
+		print(host, 'Esta Abajo')
+
+	t = threading.current_thread()
+	print(f'Hilo {t.name} con valor previo {local.my_value}')
+
+	barrier.wait()
+	print(f'Hilo {t.name} con valor final {local.my_value}')
 
 
 
